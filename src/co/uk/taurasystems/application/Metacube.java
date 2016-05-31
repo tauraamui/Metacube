fb8dabdd704d4c6caa778237f027f8b80339ce90
tauraamui <aplewis95@gmail.com>
2016-05-31 18:34:37 +0100

    Updated loading method
    Changed the loading arrangements

--- src/co/uk/taurasystems/application/Metacube.java
+++ src/co/uk/taurasystems/application/Metacube.java
@@ -3,22 +3,50 @@ package co.uk.taurasystems.application;
 
 import java.io.IOException;
 
+import co.uk.taurasystems.application.ui.newdialog.NewDialogController;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.stage.Modality;
 import javafx.stage.Stage;
 
 
 public class Metacube extends Application {
+	
+	private static Stage primaryStage;
+	
 	@Override
 	public void start(Stage primaryStage) {
+		this.primaryStage = primaryStage;
+		loadMainWindow();
+	}
+	
+	public static void loadMainWindow() {
 		try {
-			Parent root = FXMLLoader.load(getClass().getResource("\\ui\\root\\Root.fxml"));
+			Parent root = FXMLLoader.load(Metacube.class.getResource("\\ui\\root\\Root.fxml"));
 			Scene scene = new Scene(root);
 			primaryStage.setTitle("Metacube");
 			primaryStage.setScene(scene);
 			primaryStage.show();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public static void loadNewDialog() {
+		try {
+			FXMLLoader fxmlLoader = new FXMLLoader(NewDialogController.class.getResource("NewDialog.fxml"));
+			Parent root = (Parent)fxmlLoader.load();
+			Scene scene = new Scene(root);
+			Stage dialog = new Stage();
+			dialog.setScene(scene);
+			dialog.initModality(Modality.APPLICATION_MODAL);
+			dialog.setResizable(false);
+			dialog.setTitle("New...");
+			dialog.sizeToScene();
+			((NewDialogController)fxmlLoader.getController()).setStage(dialog);
+			dialog.show();
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
--- src/co/uk/taurasystems/application/ui/root/RootController.java
+++ src/co/uk/taurasystems/application/ui/root/RootController.java
@@ -1,19 +1,13 @@
 package co.uk.taurasystems.application.ui.root;
 
-import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
-import co.uk.taurasystems.application.ui.newdialog.NewDialogController;
+import co.uk.taurasystems.application.Metacube;
 import javafx.fxml.FXML;
-import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
-import javafx.scene.Parent;
-import javafx.scene.Scene;
 import javafx.scene.control.MenuBar;
 import javafx.scene.control.MenuItem;
-import javafx.stage.Modality;
-import javafx.stage.Stage;
 
 public class RootController implements Initializable {
 
@@ -45,25 +39,6 @@ public class RootController implements Initializable {
 		//		assert menuBar != null : "fx:id=\"menuBar\"was not injected: check your FXML file 'Root.fxml'.";
 		//		assert menuCloseButton != null : "fx:id=\"menuCloseButton\"was not injected: check your FXML file 'Root.fxml'.";
 		menuCloseButton.setOnAction(e -> System.exit(0));
-		menuNewButton.setOnAction(e -> loadNewDialogTest());
-	}
-
-	private void loadNewDialogTest() {
-		FXMLLoader newDialogLoader = new FXMLLoader();
-		newDialogLoader.setLocation(NewDialogController.class.getResource("NewDialog.fxml"));
-		Parent newDialog;
-		try {
-			newDialog = (Parent)newDialogLoader.load();
-		} catch (IOException e) {
-			e.printStackTrace();
-			return;
-		}
-		Stage dialog = new Stage();
-		dialog.initModality(Modality.APPLICATION_MODAL);
-		dialog.setResizable(false);
-		dialog.setTitle("New...");
-		dialog.setScene(new Scene(newDialog));
-		dialog.sizeToScene();
-		dialog.show();
+		menuNewButton.setOnAction(e -> Metacube.loadNewDialog());
 	}
 }
